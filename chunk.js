1b59eb441fc6d4393e10614e6ff54897dac9e055
Noa Kim <noanonoa@gmail.com>
2021-05-05 11:15:32 -0700

    modifies array into chunked subarrays and test to check if it works as intended

---
+++ chunk.js
@@ -0,0 +1,36 @@
+// Array Chunking
+// Split an array into chunked arrays of a specified length
+// example: chunkArray([1,2,3,4,5,6,7], 3) --> [[1,2,3], [4,5,6], [7]]
+
+const chunkArray = (array, length) => {
+  // index starts at the beginning of the array
+  let index = 0
+  // and chunked array of sub arrays which remains to be determined
+  let chunked = []
+  while (index < array.length) {
+    // a new sub array starts at the beginning of index up to the specified length
+    const subArray = array.slice(index, (index + length))
+    // push it to the chunked array
+    chunked.push(subArray)
+    // index on the next iteration needs to start from where the previous subarray ended
+    index += length;
+  }
+  return chunked
+}
+
+const chunkArr = (array, length) => {
+  let chunked = []
+  array.forEach(value => {
+    const lastValue = chunked[chunked.length - 1]
+    if (!lastValue || lastValue.length === length) {
+      chunked.push([value])
+    } else {
+      lastValue.push(value)
+    }
+  })
+  return chunked
+}
+
+console.log(chunkArr([1,2,3,4,5,6,7], 3))
+
+module.exports = chunkArray
